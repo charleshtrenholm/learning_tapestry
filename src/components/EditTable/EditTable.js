010c7b1ca94db28ff51c1e1f653f39aae23e7fb4
Charlie Trenholm <charleshtrenholm@gmail.com>
2019-09-01 15:38:53 -0700

    get additional information about page edits from wiki API

--- src/components/EditTable/EditTable.js
+++ src/components/EditTable/EditTable.js
@@ -9,20 +9,24 @@ function EditTable() {
         height: '500px',
         backgroundColor: 'red'
     };
-
+    const d = new Date();
+    d.setHours(d.getHours() - 1);
     const [edits, setEdits] = useState([]);
+    const [date, setDate] = useState(d);
 
     useEffect(() => {
         const options = {
             method: 'GET',
         }
         fetch(
-            'https://en.wikipedia.org/w/api.php?&origin=*&action=query&list=recentchanges&format=json&rcstart=2019-08-29T10:59:20Z&rcnamespace=0&rcshow=!minor%7C!bot%7C!anon%7C!redirect&rclimit=500&rcdir=newer',
+            `https://en.wikipedia.org/w/api.php?` +
+            `&origin=*&action=query&list=recentchanges&format=json&rcstart=${date.toISOString()}` + 
+            `&rcprop=title|ids|user|userid` +
+            `&rcnamespace=0&rcshow=!minor%7C!bot%7C!anon%7C!redirect&rclimit=50&rcdir=newer`,
             options
         )
         .then(response => response.json())
         .then(result => {
-            console.log(result);
             setEdits(result.query.recentchanges);
         });
     })
@@ -34,18 +38,28 @@ function EditTable() {
                 {edits.length ? 
                     (<table className="table table-striped table-dark">
                         <thead>
-                            <th>Title</th>
-                            <th>revID</th>
-                            <th>old revID</th>
-                            <th>Edited on</th>
+                            <tr>
+                                <th>Title</th>
+                                <th>revID</th>
+                                <th>old revID</th>
+                                <th>Edited on</th>
+                                <th>View</th>
+                            </tr>
                         </thead>
                         <tbody>
                             {edits.map(edit => (
-                                <tr>
+                                <tr key={edit.pageid}>
                                     <td>{edit.title}</td>
                                     <td>{edit.revid}</td>
                                     <td>{edit.old_revid}</td>
                                     <td>{edit.timestamp}</td>
+                                    <th>
+                                        <Link to={`/e/${edit.pageid}`}>
+                                            <button className="btn btn-info">
+                                            View
+                                            </button>
+                                        </Link>
+                                    </th>
                                 </tr>
                             )              
                         )}
