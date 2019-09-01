da111deaf9a360e4ce815ce452df8c097b8b0516
Charlie Trenholm <charleshtrenholm@gmail.com>
2019-09-01 14:58:36 -0700

    load wikipedia api data into UI components

--- src/components/EditTable/EditTable.js
+++ src/components/EditTable/EditTable.js
@@ -20,18 +20,43 @@ function EditTable() {
             'https://en.wikipedia.org/w/api.php?&origin=*&action=query&list=recentchanges&format=json&rcstart=2019-08-29T10:59:20Z&rcnamespace=0&rcshow=!minor%7C!bot%7C!anon%7C!redirect&rclimit=500&rcdir=newer',
             options
         )
-        .then(result => console.log(result.json()));
+        .then(response => response.json())
+        .then(result => {
+            console.log(result);
+            setEdits(result.query.recentchanges);
+        });
     })
 
-    return(
-        <div className="col-12">
-            <NavBar />
-            {edits.length ? 
-                <div style={styles}></div>
-                : <p>loading</p>
-            }
+    return (
+        <div className="row justify-content-center">
+            <div className="col-10">
+                <NavBar />
+                {edits.length ? 
+                    (<table className="table table-striped table-dark">
+                        <thead>
+                            <th>Title</th>
+                            <th>revID</th>
+                            <th>old revID</th>
+                            <th>Edited on</th>
+                        </thead>
+                        <tbody>
+                            {edits.map(edit => (
+                                <tr>
+                                    <td>{edit.title}</td>
+                                    <td>{edit.revid}</td>
+                                    <td>{edit.old_revid}</td>
+                                    <td>{edit.timestamp}</td>
+                                </tr>
+                            )              
+                        )}
+                        </tbody>
+                    </table>
+                    )
+                    : <p>loading</p>
+                }
+            </div>
         </div>
-    );
+    ); 
 }
 
 export default EditTable;
