cda30c8a1d9544eb89c0bbf4166fad3241ffc248
Charlie Trenholm <charleshtrenholm@gmail.com>
2019-09-01 21:36:42 -0700

    add sort by and start time functionality in nav bar UI

--- src/components/EditTable/EditTable.js
+++ src/components/EditTable/EditTable.js
@@ -14,7 +14,9 @@ function EditTable() {
     // set default date to 1 hour ago
 
     const [edits, setEdits] = useState([]);
-    const [date, setDate] = useState(d);
+    const [startTime, setStartTime] = useState(d);
+    const [pagination, setPagination] = useState();
+    const [sortBy, setSortBy] = useState('newer');
 
     useEffect(() => {
         const options = {
@@ -22,59 +24,84 @@ function EditTable() {
         }
         fetch(
             `https://en.wikipedia.org/w/api.php?` +
-            `&origin=*&action=query&list=recentchanges&format=json&rcstart=${date.toISOString()}` + 
+            `&origin=*&action=query&list=recentchanges&format=json&rcstart=${startTime.toISOString()}` + 
             `&rcprop=title|ids|user|userid|comment|parsedcomment|loginfo|tags` +
-            `&rcnamespace=0&rcshow=!minor%7C!bot%7C!anon%7C!redirect&rclimit=50&rcdir=newer`,
+            `&rcnamespace=0&rcshow=!minor%7C!bot%7C!anon%7C!redirect&rclimit=50&rcdir=${sortBy}`,
             options
         )
         .then(response => response.json())
-        // .then(response => console.log(response.json()))
         .then(result => {
             setEdits(result.query.recentchanges);
         });
     })
 
+    const handleStartTime= $event => {
+        if($event) { // clear button emits null
+            const split = $event.split(':');
+            const hours = split[0];
+            const minutes = split[1];
+            const d = new Date();
+            d.setHours(hours, minutes)
+            setStartTime(d);
+        }
+    }
+
+    const newStartTime = event => console.log(event, typeof event);
+
     return (
         <div className="row justify-content-center">
             <div className="col-10">
-                <NavBar />
+                <NavBar
+                    onSortByChange={$event => setSortBy($event)}
+                    onStartTimeChange={$event => handleStartTime($event)}
+                />
                 {edits.length ? 
-                    (<table className="table table-striped table-dark">
-                        <thead>
-                            <tr>
-                                <th>Title</th>
-                                <th>revID</th>
-                                <th>Page ID</th>
-                                <th>Edited By</th>
-                                <th>View</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {edits.map(edit => (
-                                <tr key={edit.rcid}>
-                                    <td>{edit.title}</td>
-                                    <td>{edit.revid}</td>
-                                    <td>{edit.pageid}</td>
-                                    <td>{edit.user}</td>
-                                    <th>
-                                        <Link to={{
-                                            pathname: `/e/${edit.rcid}`,
-                                            state: {
-                                                comment: edit.comment,
-                                                title: edit.title,
-                                                user: edit.user,
-                                            }
-                                        }}>
-                                            <button className="btn btn-info">
-                                            View
-                                            </button>
-                                        </Link>
-                                    </th>
+                    (<div>
+                        <table className="table table-striped table-dark">
+                            <thead>
+                                <tr>
+                                    <th>Title</th>
+                                    <th>revID</th>
+                                    <th>Page ID</th>
+                                    <th>Edited By</th>
+                                    <th>View</th>
                                 </tr>
-                            )              
-                        )}
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                {edits.map(edit => (
+                                    <tr key={edit.rcid}>
+                                        <td>{edit.title}</td>
+                                        <td>{edit.revid}</td>
+                                        <td>{edit.pageid}</td>
+                                        <td>{edit.user}</td>
+                                        <th>
+                                            <Link to={{
+                                                pathname: `/e/${edit.rcid}`,
+                                                state: {
+                                                    comment: edit.comment,
+                                                    title: edit.title,
+                                                    user: edit.user,
+                                                }
+                                            }}>
+                                                <button className="btn btn-info">
+                                                View
+                                                </button>
+                                            </Link>
+                                        </th>
+                                    </tr>
+                                )              
+                            )}
+                            </tbody>
+                        </table>
+                        <div className="row">
+                            <span 
+                                className="col-2 offset-1" 
+                                style={{border: '1px solid grey', borderRadius: '10px', marginBottom: '20px'}}
+                                onClick={setPagination}
+                                >&laquo; previous</span>
+                            <span className="col-2 offset-6" style={{border: '1px solid grey', borderRadius: '10px', marginBottom: '20px'}}>&raquo; next</span>
+                        </div>
+                    </div>
                     )
                     : <p>loading</p>
                 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import './NavBar.css';
+import TimePicker from 'react-time-picker';
 
-function NavBar() {
+function NavBar({ onSortByChange, onStartTimeChange, onLimitPageChange }) {
+
     return(
-        <p>NavBar</p>
+        <div className="row top-nav">
+            <div className="col-4">
+                <label htmlFor="sortby">Sort By</label>
+                <select 
+                    name="sortby" 
+                    className="form-control" 
+                    onChange={$event => onSortByChange($event.target.value)}
+                    >
+                    <option value="newer">newer</option>
+                    <option value="older">older</option>
+                </select>
+            </div>
+            <div className="col-4">
+                <label htmlFor="starttime">Start Time</label>
+                <TimePicker
+                    maxTime={new Date()}
+                    clearIcon="clear"
+                    name="starttime"
+                    className="form-control"
+                    onChange={$event => onStartTimeChange($event)}
+                    disableClock={true}
+                />
+            </div>
+            {/* <div className="col-4">
+                <label htmlFor="maxpagesize">Max Page Size</label>
+                <select
+                    name="maxpagesize"
+                    className="form-control" */}
+
+        </div>
     );
 } 
 
